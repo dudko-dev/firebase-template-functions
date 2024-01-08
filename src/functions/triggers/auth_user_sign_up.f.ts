4231406b67d86e1f196fc8a1e3e09d178113fb4d
Siarhei Dudko <slavianich@gmail.com>
2024-01-08 20:23:05 +0400

    update signin triogger

--- src/functions/triggers/auth_user_sign_up.f.ts
+++ src/functions/triggers/auth_user_sign_up.f.ts
@@ -1,9 +1,17 @@
 import { runWith } from '../../services/FirebaseFunctions';
 import { handler } from './auth_user_sign_up';
 
+// needs to upgrade to Firebase Auth with Identity Platform
+// module.exports = runWith({
+//   memory: '256MB',
+//   timeoutSeconds: 60,
+// })
+//   .auth.user()
+//   .beforeCreate(handler);
+
 module.exports = runWith({
   memory: '256MB',
   timeoutSeconds: 60,
 })
   .auth.user()
-  .beforeCreate(handler);
+  .onCreate(handler);
